6c9ca7904f60b89aa619e83d7bfd408ff5cabdd0
tndud042713 <tndud042713@gmail.com>
2020-11-17 00:08:21 +0900

    퀴즈 해결

---
+++ 평일_1900자바수업_변수영/day01/src/quiz/Quiz01.java
@@ -0,0 +1,15 @@
+package quiz;
+
+public class Quiz01 {
+public static void main(String[] args) {
+	System.out.println("========================================");
+	System.out.println("이름\t나이\t전화번호\t\t회비\t");
+	System.out.println("========================================");
+	System.out.println("홍길동\t15\t3672-1234\t\\20000\t");
+	System.out.println("고길동\t15\t2238-1234\t\\30000\t");
+	System.out.println("김말이\t15\t1234-1234\t\\50000\t");
+	System.out.println("----------------------------------------");
+	System.out.println("총합계\t\t\t\t\\100000");
+	System.out.println("----------------------------------------");
+}
+}
