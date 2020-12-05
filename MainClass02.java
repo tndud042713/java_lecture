3a5668d6358d3f8642bfbeff9a9d8381eee4c8dd
ByunSuyoung <70098543+tndud042713@users.noreply.github.com>
2020-12-05 23:52:59 +0900

    로또 난수 생성기
    일단 6개가 다 다른숫자로 출력되기 위해서 논리 수정
    두번째는 6개의 숫자를 나오게 할 때까지 무한루프를 돌린 후 6개의 숫자가 나오면 출력되도록 
    코드 수정

---
+++ MainClass02.java
@@ -0,0 +1,28 @@
+package day09;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class MainClass02 {
+	public static void main(String[] args) {
+
+		System.out.println("오늘의 로또번호는 ");
+		// 로또번호 추첨기
+
+		while (true) {
+			HashSet num = new HashSet();
+			for (int i = 0; i < 6; i++) {
+
+				int k = (int) (Math.random() * 45 + 1);
+				num.add(k);
+			}
+			if (num.size() == 6) {
+				System.out.println(num);
+				break;
+			} else {
+				continue;
+			}
+		}
+
+	}
+}
